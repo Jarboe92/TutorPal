009bee8c4f0da899733d03880e4bbd337d972656
Raghav Chetal <raghav.chetal@gmail.com>
2019-12-03 01:43:49 -0500

    Added buttons for redeem points, no functionality

--- app/src/main/java/com/example/tutorpal/PointsActivity.java
+++ app/src/main/java/com/example/tutorpal/PointsActivity.java
@@ -2,7 +2,10 @@ package com.example.tutorpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PointsActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class PointsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_points);
+
+        final Button apple = (Button) findViewById(R.id.apple);
+        final Button nike = (Button) findViewById(R.id.nike);
+        final Button adidas = (Button) findViewById(R.id.adidas);
+        final Button dell = (Button) findViewById(R.id.dell);
+
+
+        apple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PointsActivity.class);
+                startActivity(intent);
+
+//                Snackbar mySnackbar = Snackbar.make(view, stringId, duration);
+            }
+        });
     }
 }
