5909e0c6167b4b1091f8d8e2f31e97cff94ed462
sofiaFern <sofia.fernandezpoyatos@gmail.com>
2019-12-03 00:41:17 -0500

    oops adding more

---
+++ app/src/main/java/com/example/tutorpal/StartActivity.java
@@ -0,0 +1,29 @@
+package com.example.tutorpal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        final Button register = (Button) findViewById(R.id.RegisterButton);
+        final Button logIn = (Button) findViewById(R.id.LogInButton);
+
+        //log in button will take you to the points page for now, we will assume user already exists
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
