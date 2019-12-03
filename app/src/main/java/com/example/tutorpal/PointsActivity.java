9794c0840c20711af65cd9cd3ef68661a02ebf4c
Raghav Chetal <raghav.chetal@gmail.com>
2019-12-03 01:53:24 -0500

    added redeem point button functionality

--- app/src/main/java/com/example/tutorpal/PointsActivity.java
+++ app/src/main/java/com/example/tutorpal/PointsActivity.java
@@ -2,10 +2,12 @@ package com.example.tutorpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class PointsActivity extends AppCompatActivity {
 
@@ -19,14 +21,51 @@ public class PointsActivity extends AppCompatActivity {
         final Button adidas = (Button) findViewById(R.id.adidas);
         final Button dell = (Button) findViewById(R.id.dell);
 
-
         apple.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PointsActivity.class);
-                startActivity(intent);
+                Context context = getApplicationContext();
+                CharSequence text = "Apple Code: apple_code";
+                int duration = Toast.LENGTH_SHORT;
 
-//                Snackbar mySnackbar = Snackbar.make(view, stringId, duration);
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        nike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Nike Code: nike_code";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        adidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Adidas Code: adidas_code";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        dell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Dell Code: dell_code";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
     }
