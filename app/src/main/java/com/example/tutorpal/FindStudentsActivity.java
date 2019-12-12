27e3e20ae9a97054394c1766fa25de81fa805dac
Bobby <rmjarboe1@gmail.com>
2019-12-11 22:46:42 -0500

    working on having a user select a date, start and end time for a tutor appt.

--- app/src/main/java/com/example/tutorpal/FindStudentsActivity.java
+++ app/src/main/java/com/example/tutorpal/FindStudentsActivity.java
@@ -1,8 +1,17 @@
 package com.example.tutorpal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class FindStudentsActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/tutorpal/FindTutorActivity.java
+++ app/src/main/java/com/example/tutorpal/FindTutorActivity.java
@@ -2,13 +2,52 @@ package com.example.tutorpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class FindTutorActivity extends AppCompatActivity {
+
+    private static final String TAG = "FindStudentActivity";
+    EditText date;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_tutor);
+
+        date = (EditText) findViewById(R.id.dateTutor);
+        //DatePicker temp = datePickerDialog.getDatePicker();
+        //temp.setSpinnersShown(true);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+                // date picker dialog
+                datePickerDialog = new DatePickerDialog(FindTutorActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                date.setText((monthOfYear + 1) + "/"
+                                        + (dayOfMonth) + "/" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
     }
 }
