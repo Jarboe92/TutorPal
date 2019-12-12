e867e7c7ef18991f4a984809537ce65316d46f30
piripirippoppo <33378800+piripirippoppo@users.noreply.github.com>
2019-12-12 01:15:47 -0500

    FindTutorpage
    full part

--- app/src/main/java/com/example/tutorpal/FindTutorActivity.java
+++ app/src/main/java/com/example/tutorpal/FindTutorActivity.java
@@ -1,14 +1,102 @@
 package com.example.tutorpal;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FindTutorActivity extends AppCompatActivity {
 
+    int [] images = {R.drawable.raghav, R.drawable.robert, R.drawable.sofia,
+            R.drawable.jina, R.drawable.btrix, R.drawable.jerry};
+
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_tutor);
+
+        listView = findViewById(R.id.listview);
+
+        MyAdapter customAdapter = new MyAdapter();
+        listView.setAdapter(customAdapter);
+
+        //handle item clicks
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //Raghav
+                if(position == 0) {
+                    Intent intent = new Intent();
+                    intent.setClass(FindTutorActivity.this, RaghavActivity.class);
+                    startActivity(intent);
+                }
+
+                if(position == 1){
+                    Intent intent = new Intent();
+                    intent.setClass(FindTutorActivity.this, RobertActivity.class);
+                    startActivity(intent);
+                }
+
+                if(position == 2){
+                    Intent intent = new Intent();
+                    intent.setClass(FindTutorActivity.this, SofiaActivity.class);
+                    startActivity(intent);
+                }
+
+                if(position == 3){
+                    Intent intent = new Intent();
+                    intent.setClass(FindTutorActivity.this, JinaActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+
+
+
+    class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = getLayoutInflater().inflate(R.layout.tutor_layout, null);
+            ImageView imageView = view.findViewById(R.id.avatar);
+            imageView.setImageResource(images[position]);
+
+            return view;
+        }
     }
 }
---
+++ app/src/main/java/com/example/tutorpal/SofiaActivity.java
@@ -0,0 +1,80 @@
+package com.example.tutorpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+public class SofiaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener{
+
+    final String [] start = {"Start Time", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",
+            "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM"};
+
+    final String [] end = {"End Time", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM",
+            "6PM", "7PM", "8PM", "9PM", "10PM", "11PM", "12AM"};
+
+    ImageView imageView1;
+    ImageView imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sofia);
+
+        imageView1 = findViewById(R.id.back);
+        imageView1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SofiaActivity.this,
+                        FindTutorActivity.class);
+
+                SofiaActivity.this.startActivity(intent);
+            }
+        });
+
+        imageView2 = findViewById(R.id.next);
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SofiaActivity.this,
+                        PaymentActivity.class);
+
+                SofiaActivity.this.startActivity(intent);
+            }
+        });
+
+
+        Spinner spinner1 = findViewById(R.id.spinner);
+
+        ArrayAdapter<String> one = new ArrayAdapter<>(this,
+                R.layout.support_simple_spinner_dropdown_item, start);
+        spinner1.setAdapter(one);
+
+        Spinner spinner2 = findViewById(R.id.spinner2);
+
+        ArrayAdapter<String> two = new ArrayAdapter<>(this,
+                R.layout.support_simple_spinner_dropdown_item, end);
+        spinner2.setAdapter(two);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
